dabe6bd0a601aca71b6ae24d616153b30bf31869
Stanisław Małolepszy <stas@piesku.com>
2022-02-18 16:24:32 +0100

    Parse lines as paragraphs

--- index.php
+++ index.php
@@ -14,9 +14,10 @@
 	$placeholders = implode(',', array_fill(0, count($_GET), '?'));
 	$statement = $pdo->prepare(
 		"SELECT
+			pages.slug as slug,
 			pages.title as title,
 			revisions.body as body,
-			MAX(revisions.time_created) as time_created
+			MAX(revisions.time_created) as last_modified
 		FROM
 			revisions
 			JOIN pages ON revisions.page_id = pages.id
@@ -29,11 +30,29 @@
 
 	$statement->execute(array_keys($_GET));
 ?>
+
+<?php
+	function into_html($body)
+	{
+		foreach(explode("\n", $body) as $line) {
+			yield "<p>" . htmlentities($line) . "</p>";
+		}
+	}
+?>
 <body>
-	<?php foreach($statement as $page) { ?>
+	<?php foreach($statement as $page) {
+		$slug = $page["slug"];
+		$title = $page["title"];
+		$body = $page["body"];
+		$last_modified = $page["last_modified"];
+
+		$action = $_GET[$slug];
+	?>
 		<article>
-			<h1><?= $page["title"] ?></h1>
-			<?= $page["body"] ?>
+			<h1><?= $title ?></h1>
+			<?php foreach(into_html($body) as $elem) {
+				echo $elem;
+			} ?>
 		</article>
 	<?php } ?>
 </body>
