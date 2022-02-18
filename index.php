ff3913aaf7badb764cbba267ce1a79ffa2848d4e
Stanisław Małolepszy <stas@piesku.com>
2022-02-18 17:26:02 +0100

    switch views based on

--- index.php
+++ index.php
@@ -38,44 +38,66 @@ class Page
 ?>
 
 <?php
+
+function view_read($slug)
+{
 	$pdo = new PDO('sqlite:./wk.sqlite');
+	$statement = $pdo->prepare(
+		"SELECT
+			pages.slug as slug,
+			pages.title as title,
+			revisions.body as body,
+			MAX(revisions.time_created) as last_modified
+		FROM
+			revisions
+			JOIN pages ON revisions.page_id = pages.id
+		WHERE
+			pages.slug = ?
+		GROUP BY
+			pages.slug
+		;"
+	);
 
-	foreach($_GET as $slug => $action) {
-		$statement = $pdo->prepare(
-			"SELECT
-				pages.slug as slug,
-				pages.title as title,
-				revisions.body as body,
-				MAX(revisions.time_created) as last_modified
-			FROM
-				revisions
-				JOIN pages ON revisions.page_id = pages.id
-			WHERE
-				pages.slug = ?
-			GROUP BY
-				pages.slug
-			;"
-		);
+	$statement->execute(array($slug));
+	$statement->setFetchMode(PDO::FETCH_CLASS, 'Page');
+	$page = $statement->fetch();
 
-		$statement->execute(array($slug));
-		$statement->setFetchMode(PDO::FETCH_CLASS, 'Page');
-		$page = $statement->fetch();
+	if (!$page) {
+		view_not_found($slug);
+	} else {
+		view_render($page);
+	}
+}
 
-		if (!$page) {
-			?>
-				<article>
-					<h1>Page Not Found</h1>
-					<p><?=$slug?> doesn't exist yet. <a href="?<?=$slug?>=edit">Create?</a></p>
-				</article>
-			<?php
-			continue;
-		}
-	?>
-		<article>
-			<h1><?=$page->title?></h1>
-			<?php foreach($page->IntoHtml() as $elem) {
-				echo $elem;
-			} ?>
-		</article>
-	<?php } ?>
+function view_not_found($slug)
+{ ?>
+	<article>
+		<h1>Page Not Found</h1>
+		<p><?=$slug?> doesn't exist yet. <a href="?<?=$slug?>=edit">Create?</a></p>
+	</article>
+<?php }
+
+function view_render($page)
+{ ?>
+	<article>
+		<h1><?=$page->title?></h1>
+		<?php foreach($page->IntoHtml() as $elem) {
+			echo $elem;
+		} ?>
+	</article>
+<?php }
+
+foreach($_GET as $slug => $action) {
+	switch ($action) {
+		case "create":
+		case "edit":
+		case "refs":
+			break;
+		case "read":
+		default:
+			view_read($slug);
+	}
+}
+
+?>
 </body>
